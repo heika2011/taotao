25cb035df66b44b8331a3243555e84d0354d9aa3
heika2011 <965659478@qq.com>
2020-01-06 16:20:23 +0800

    2020年1月6日 redis配置上传 --谢

---
+++ tt-common/src/main/java/cn/tt/common/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package cn.tt.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import redis.clients.jedis.Jedis;
+
+/**
+ * Redis配置类
+ * 谢
+ */
+@Configuration
+@PropertySource("classpath:/properties/redis.properties")
+public class RedisConfig {
+
+    @Value("${redis.url}")
+    private String url;
+    @Value("${redis.port}")
+    private Integer port;
+
+    @Bean
+    public Jedis jedis(){
+        return new Jedis(url,port);
+    }
+}
--- tt-user/src/main/java/cn/tt/user/service/TestService.java
+++ tt-user/src/main/java/cn/tt/user/service/TestService.java
@@ -1,12 +1,19 @@
 package cn.tt.user.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
 @Service
 public class TestService implements cn.tt.common.service.TestService {
 
+    @Autowired
+    private Jedis jedis;
+
     @Override
     public String test() {
+        jedis.set("1","2++++++++++++++++++++++++++++");
+        System.out.println(jedis.get("1"));
         return "hello";
     }
 }
